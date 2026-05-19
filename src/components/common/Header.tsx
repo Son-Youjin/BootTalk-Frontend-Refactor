c5b2da1fd372c9f0cd61373da56ef0ec3635062e
SonYoujin <wlsl423@naver.com>
2026-05-19 16:19:00 +0900

    🐛 모바일 헤더 알림 버튼 레이아웃 이슈 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -55,19 +55,25 @@ const Header = () => {
         onChange={(e) => setIsDrawerOpen(e.target.checked)}
       />
       <header className="sticky top-0 z-60 shadow-m bg-base-100 shadow-md">
-        <div className="navbar max-w-[1200px] mx-auto px-4 md:px-6 relative justify-between">
-          <div className="flex md:hidden items-center">
+        <div className="navbar max-w-[1200px] mx-auto px-4 md:px-6 items-center justify-between">
+          <div className="w-12 flex items-center md:w-auto">
             <button
-              className="btn btn-ghost"
+              className="btn btn-ghost md:hidden"
               onClick={() => setIsDrawerOpen(true)}
             >
               <Menu size={24} />
             </button>
           </div>
 
-          <div className="absolute left-1/2 -translate-x-1/2 md:static md:translate-x-0">
+          <div className="flex justify-center">
             <Link href="/">
-              <Image src="/logo.PNG" alt="로고" width={160} height={20} />
+              <Image
+                src="/logo.PNG"
+                alt="로고"
+                width={160}
+                height={20}
+                className="w-[120px] md:w-[160px]"
+              />
             </Link>
           </div>
 
@@ -88,58 +94,60 @@ const Header = () => {
             </div>
           </nav>
 
-          <div className="items-center gap-4">
-            {isAuthenticated && user ? (
-              <div className="flex items-center gap-3">
-                <div className="flex">
-                  <div className="relative">
-                    <button
-                      className="btn btn-ghost btn-circle"
-                      aria-label="알림"
+          <div className="w-12 flex justify-end items-center md:w-auto">
+            <div className="flex items-center gap-4">
+              {isAuthenticated && user ? (
+                <div className="flex items-center gap-3">
+                  <div className="flex">
+                    <div className="relative">
+                      <button
+                        className="btn btn-ghost btn-circle"
+                        aria-label="알림"
+                      >
+                        <NotificationDropdown />
+                      </button>
+                    </div>
+                    <Link
+                      href="/chat"
+                      className="hidden btn btn-ghost btn-circle"
+                      aria-label="채팅"
                     >
-                      <NotificationDropdown />
+                      <MessageCircleCode size={18} />
+                    </Link>
+                  </div>
+
+                  <div className="hidden md:block">
+                    <Link
+                      href="/mypage"
+                      className="text-sm font-medium hover:underline"
+                    >
+                      {`${myInfo?.name}님`}
+                    </Link>
+
+                    <span className="text-sm font-medium ml-3">
+                      {myInfo?.currentPoint}P
+                    </span>
+
+                    <button
+                      className="btn bg-base-100 border-none text-sm hover:text-amber-950 transition-colors"
+                      onClick={() => logout.mutate()}
+                      disabled={isMyInfoLoading || isMyInfoError}
+                    >
+                      로그아웃
                     </button>
                   </div>
-                  <Link
-                    href="/chat"
-                    className="hidden btn btn-ghost btn-circle"
-                    aria-label="채팅"
-                  >
-                    <MessageCircleCode size={18} />
-                  </Link>
                 </div>
-
-                <div className="hidden md:block">
+              ) : (
+                <div className="flex items-center gap-2">
                   <Link
-                    href="/mypage"
-                    className="text-sm font-medium hover:underline"
-                  >
-                    {`${myInfo?.name}님`}
-                  </Link>
-
-                  <span className="text-sm font-medium ml-3">
-                    {myInfo?.currentPoint}P
-                  </span>
-
-                  <button
+                    href="/login"
                     className="btn bg-base-100 border-none text-sm hover:text-amber-950 transition-colors"
-                    onClick={() => logout.mutate()}
-                    disabled={isMyInfoLoading || isMyInfoError}
                   >
-                    로그아웃
-                  </button>
+                    로그인 / 회원가입
+                  </Link>
                 </div>
-              </div>
-            ) : (
-              <div className="flex items-center gap-2">
-                <Link
-                  href="/login"
-                  className="btn bg-base-100 border-none text-sm hover:text-amber-950 transition-colors"
-                >
-                  로그인 / 회원가입
-                </Link>
-              </div>
-            )}
+              )}
+            </div>
           </div>
         </div>
       </header>
