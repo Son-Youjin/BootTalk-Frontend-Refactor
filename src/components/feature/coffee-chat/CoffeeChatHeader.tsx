abdc49c0286e1b60a5cfe96a4d7fc818676a769a
SonYoujin <wlsl423@naver.com>
2026-06-01 12:56:35 +0900

    💄 커피챗 페이지 헤더 및 안내 문구 추가

--- src/components/feature/coffee-chat/CoffeeChatHeader.tsx
+++ src/components/feature/coffee-chat/CoffeeChatHeader.tsx
@@ -2,25 +2,12 @@
 
 import React from "react";
 
-import { useRouter } from "next/navigation";
-
 const CoffeeChatHeader: React.FC = () => {
-  const router = useRouter();
-
-  const navigateToMyPage = () => {
-    router.push("/mypage?tab=mentor");
-  };
-
   return (
     <>
-      <div className="flex justify-between items-center mb-6">
-        <h1 className="text-2xl font-bold">커피챗</h1>
-        <button
-          className="btn btn-outline hover:text-amber-900 rounded-lg"
-          onClick={navigateToMyPage}
-        >
-          멘토 프로필 관리
-        </button>
+      <div className="flex-col justify-between items-center mb-6">
+        <h1 className="text-2xl font-bold">내 커피챗을 한눈에</h1>
+        <h2>신청 · 확정된 커피챗을 관리하세요.</h2>
       </div>
     </>
   );
