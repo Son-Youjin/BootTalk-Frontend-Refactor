b372d7ec94dfbb8ef1b33270866f5886c672dfb7
SonYoujin <wlsl423@naver.com>
2026-07-15 12:21:37 +0900

    🐛 CommonModal 모바일 레이아웃 여백 문제 수정

--- src/components/common/modal/CommonModal.tsx
+++ src/components/common/modal/CommonModal.tsx
@@ -27,7 +27,6 @@ const Modal: React.FC<CommonModalProps> = ({
   useEffect(() => {
     if (isOpen) {
       document.body.style.overflow = "hidden";
-      document.body.style.paddingRight = "17px";
     } else {
       document.body.style.overflow = "unset";
       document.body.style.paddingRight = "";
